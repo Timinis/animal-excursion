00fb6a5cdf969517246edc0f09159ebf0f71f166
Timinis <litimc3@gmail.com>
2018-10-02 09:55:47 -0700

    quick fix

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
 CREATE TABLE IF NOT EXISTS animals (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255) PRIMARY KEY,
   region VARCHAR(255)
 );
-INSERT INTO animals (name,region) VALUES ('Snow leopard','Asia'),('Siberian tiger','Asia'),('Lar gibbon','Asia'),('Phayre''s leaf monkey','Asia'),('Red panda','Asia'),('Giant panda','Asia'),('Pallas''s cat','Asia'),('Scaly-sided merganser','Asia'),('Sarus crane','Asia'),('Mangshan pitviper','Asia'),('Loggerhead sea turtle','Asia'), ('Chinese giant salamander','Asia')
+INSERT INTO animals (name,region) 
+VALUES 
+('Snow leopard','Asia'),('Siberian tiger','Asia'),
+('Lar gibbon','Asia'),('Phayre''s leaf monkey','Asia'),
+('Red panda','Asia'),('Giant panda','Asia'),
+('Pallas''s cat','Asia'),('Scaly-sided merganser','Asia'),
+('Sarus crane','Asia'),('Trimeresurus mangshanensis','Asia'),
+('Loggerhead sea turtle','Asia'), ('Chinese giant salamander','Asia');
