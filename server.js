3f7febed3fcc90ecb7219c698e094f9fea01f1fb
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-10-02 14:48:40 -0700

    updates to css and html

--- server.js
+++ server.js
@@ -14,37 +14,25 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-=======
 //routes
-app.get('/', (request, response) => {
-  response.sendFile('index.html', { root: './public' });
-});
+// app.get('/', (request, response) => {
+//   response.sendFile('index.html', { root: './public' });
+// });
 
-app.get('/start', (request, response) => {
-  dataPull().then(list => {
-    animalQuestionDisplay(list, response);
-  });
-});
+// app.get('/start', (request, response) => {
+//   dataPull().then(list => {
+//     animalQuestionDisplay(list, response);
+//   });
+// });
 
 // Object Creators for detail page render
 
 let questionList;
 
->>>>>>> a74b75e5bbdc1d903a0400ed88577ffb5c9845d7
 function AnimalDetail(animalResult) {
   (this.name = animalResult.title), (this.description = animalResult.extract);
 }
 
-<<<<<<< HEAD
-function animalQuestionDisplay(object) {
-  let url = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages&titles=${object}`;
-  superagent.get(url).then(result => {
-    let image = Object.values(result.body.query.pages)[0];
-    let image_url = image.thumbnail.source;
-    image_url = image_url.replace(/\/\w+px/, '/200px');
-    console.log({ name: object, image_ur: image_url });
-=======
 //function to store the questions in a object oritented format
 const animalQuestionDisplay = (array, response) => {
   let sendList = [];
@@ -62,7 +50,6 @@ const animalQuestionDisplay = (array, response) => {
         response.send(sendList);
       }
     });
->>>>>>> a74b75e5bbdc1d903a0400ed88577ffb5c9845d7
   });
 }
 
@@ -74,9 +61,6 @@ function animalDetailDisplay(search) {
   });
 }
 
-<<<<<<< HEAD
-animalQuestionDisplay(`monkey`);
-=======
 function dataPull() {
   const SQL = 'Select * from animals';
   return client.query(SQL).then(result => {
@@ -86,4 +70,3 @@ function dataPull() {
 }
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
->>>>>>> a74b75e5bbdc1d903a0400ed88577ffb5c9845d7
