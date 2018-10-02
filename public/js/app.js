cb73675ad61b6a07ffe6ce5016ade6bcdeabddf7
Timinis <litimc3@gmail.com>
2018-10-02 10:14:46 -0700

    quick layout fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 'use strict';
 $(document).ready(function() {
-  console.log('ready!!!');
+  $('#start-game').on('click', () => console.log('game started'));
 });
-$('#start-game').on('click', console.log('game started'));
